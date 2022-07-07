bf216a909084925a17313acf05c39345de631557
Khabibulix <80721211+Khabibulix@users.noreply.github.com>
2022-07-07 16:29:42 +0200

    TDD & requirements.txt update
    Adding tests

---
+++ test_crack.py
@@ -0,0 +1,18 @@
+from crack import *
+
+
+def test_crack_dict():
+    pass
+
+
+def test_crack_incr():
+    pass
+
+
+def test_crack_smart():
+    pass
+
+
+def test_generate_the_hack():
+    assert cracker.generate_the_hack(1, "abruti") == "85fa9d10dd5aed2c42004a915de00c8d" #9 à la place de 8 première position
+
